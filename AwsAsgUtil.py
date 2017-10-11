3cdfd3fd5395df4e3967cf984d4ffd7a264690fd
John Cook <john.b.cook05@gmail.com>
2017-10-10 18:16:08 -0600

    Initial Commit

---
+++ AwsAsgUtil.py
@@ -0,0 +1,67 @@
+
+from fabric.api import run,env, put, get, local, settings
+from os import path
+import time
+from fabric.colors import green as _green, yellow as _yellow
+import boto
+import boto.ec2
+import boto.ec2.autoscale
+import boto3
+from pprint import pprint
+
+
+class AwsAsgUtilities:
+  
+    
+    def basictest(self, m):
+        m = "new message"
+        return m
+     
+
+    
+
+    """Method to get instances attached to an Autoscaling Group"""
+    def get_asginstances(self, profile):
+   
+
+        session = boto3.Session(profile_name=profile)
+        asginstances = {}
+        client = session.client('autoscaling')
+        
+        response = client.describe_auto_scaling_instances()
+
+        for i in response["AutoScalingInstances"]:
+            print(i["AutoScalingGroupName"], i["InstanceId"])
+            asginstances.setdefault(i["AutoScalingGroupName"],[]).append(i["InstanceId"])
+        
+
+
+        return asginstances
+   
+    """Method to delete an ASG with a ForceDelete=True"""
+    def delete_autoscaling_group(self, asgname, profile):
+        session = boto3.Session(profile_name=profile)
+        client = session.client('autoscaling')
+        response = client.delete_auto_scaling_group(
+        AutoScalingGroupName=asgname,
+        ForceDelete=True
+        )
+
+    """Method to delete a Launch Configuration by name"""
+    def delete_launch_configuration(self, lcname, profile):
+        session = boto3.Session(profile_name=profile)
+        client = session.client('autoscaling')
+        response = client.delete_launch_configuration(
+        LaunchConfigurationName=lcname,
+      
+        )
+
+        
+
+   
+           
+
+         
+
+        
+
---
+++ AwsRoute53Util.py
@@ -0,0 +1,74 @@
+
+from fabric.api import run,env, put, get, local, settings
+from os import path
+import time
+from fabric.colors import green as _green, yellow as _yellow
+import boto
+import boto.ec2
+import boto.ec2.autoscale
+import boto3
+from pprint import pprint
+
+
+class AwsRoute53Utilities:
+    env.user = 'ec2-user'
+    groupnames = list()
+    servers = list()
+    env.roledefs['as_servers'] = list()
+    
+    
+    def basictest(self, m):
+        m = "new message"
+        return m
+    """Method to get Hosted Zone ID"""
+    def gethostedzoneid(self, domainname, profile):
+        ## Setup boto3 client with route53
+
+        session = boto3.Session(profile_name=profile)
+        boto3client =  session.client('route53')
+
+        zones = boto3client.list_hosted_zones()
+
+        #for k, v in zones.iteritems():
+        #    print(k, v)
+        for x in zones["HostedZones"]:
+            if x["Name"] == domainname:
+                ## May want to refactor
+                zid = x["Id"].split('/')[2]
+
+        print(zid)
+        return zid
+
+
+          
+    """ Method to Update IP Address of a recordset""" 
+    def updateArecordset(self, hostedzoneid, record_name, ipaddress, profile):
+
+
+        session = boto3.Session(profile_name=profile)
+        ## Setup boto3 client with route53
+        boto3client =  session.client('route53')
+        
+        ## Perform UPSERT Action
+        response = boto3client.change_resource_record_sets(
+        HostedZoneId=hostedzoneid,
+        ChangeBatch={
+        "Comment": "Automatic DNS update",
+        "Changes": [
+            {
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": record_name,
+                    "Type": "A",
+                    "TTL": 300,
+                    "ResourceRecords": [
+                        {
+                            "Value": ipaddress
+                        },
+                    ],
+                }
+            },
+        ]
+                     }
+                )
+        print(response)
